20153acbf94bf9e4b31a36f2930bdfeebfccc1b4
Matsadura <zidanditoo1@gmail.com>
2025-06-15 00:05:11 +0100

    [FIX] invalid var names in export

--- execution/builtins/export_builtin.c
+++ execution/builtins/export_builtin.c
@@ -13,6 +13,32 @@
 #include "../../includes/execution.h"
 
 /**
+ * ft_is_valid_identifier - Checks if a string is a valid identifier for an environment variable
+ * @str: The string to check
+ * Returns: 1 if valid, 0 otherwise
+ */
+static int	ft_is_valid_identifier(const char *str)
+{
+	char	*eq_pos;
+	int		name_len;
+
+	if (str == NULL || *str == '\0' || ft_isdigit(*str) == 1)
+		return (0);
+	eq_pos = ft_strchr(str, '=');
+	if (eq_pos != NULL)
+	{
+		name_len = eq_pos - str;
+		while (name_len-- > 0)
+		{
+			if (ft_isalnum(*str) == 0 && *str != '_')
+				return (0);
+			str++;
+		}
+	}
+	return (1);
+}
+
+/**
  * join_env_var - Joins a name and value into an environment variable string
  * @name: The name of the environment variable
  * @value: The value of the environment variable
@@ -74,18 +100,20 @@ static void	set_env_var(char *arg, char **env)
  */
 int	export_builtin(char **args, char **env)
 {
-	int	i;
-	int	ret;
-
+	int (i), (ret);
 	if (args == NULL || args[1] == NULL)
-	{
-		ft_dprintf(STDERR, "export: No arguments provided\n");
-		return (1);
-	}
+		return (ft_dprintf(STDERR, "export: No arguments provided\n"), 1);
 	i = 1;
 	ret = 0;
 	while (args[i])
 	{
+		if (ft_is_valid_identifier(args[i]) == 0)
+		{
+			ft_dprintf(2, "export: '%s': not a valid identifier\n", args[i]);
+			ret = 1;
+			i++;
+			continue ;
+		}
 		if (ft_strchr(args[i], '=') == NULL)
 		{
 			i++;
