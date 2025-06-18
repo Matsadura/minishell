dcede37cdd412172310606c83c1eb9896ad47fdc
Matsadura <zidanditoo1@gmail.com>
2025-06-18 21:57:12 +0100

    [FIX] unset invalid identifier

--- execution/builtins/unset_builtin.c
+++ execution/builtins/unset_builtin.c
@@ -13,6 +13,24 @@
 #include "../../includes/execution.h"
 
 /**
+ * is_valid_identifier - Checks if a string is a valid identifier
+ * @str: The string to check
+ * Returns: 1 if valid, 0 otherwise
+ */
+static int	is_valid_identifier(const char *str)
+{
+	if (str == NULL || *str == '\0' || ft_isdigit(*str) == 1)
+		return (0);
+	while (*str)
+	{
+		if (!ft_isalnum(*str) && *str != '_')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
  * remove_env_var - Removes an environment variable from the env array
  * @arg: The argument containing the variable name to remove
  * @env: The current environment variables
@@ -59,6 +77,12 @@ int	unset_builtin(char **args, char **env)
 	i = 1;
 	while (args[i])
 	{
+		if (is_valid_identifier(args[i]) == 0)
+		{
+			ft_dprintf(2, "unset: '%s': not a valid identifier\n", args[i]);
+			i++;
+			continue ;
+		}
 		remove_env_var(args[i], env);
 		i++;
 	}
