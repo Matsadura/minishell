414a21e0fecb4b36452ea65d2846983d2262b2df
Matsadura <zidanditoo1@gmail.com>
2025-06-21 11:04:36 +0100

    [FIX] exit status overflow case

--- execution/builtins/exit_builtin.c
+++ execution/builtins/exit_builtin.c
@@ -13,6 +13,32 @@
 #include "../../includes/execution.h"
 
 /**
+ * handle_exit_arg - Handles the argument for the exit command
+ * @args: The arguments passed to the exit command
+ * Return: 0 if the argument is valid, 1 if it is invalid
+ */
+static int	handle_exit_arg(char **args)
+{
+	if (ft_isnumber(args[1]) == 0)
+	{
+		ft_dprintf(STDERR, "exit\nexit: %s: numeric argument required\n", args[1]);
+		g_exit_status = 2;
+	}
+	else
+	{
+		g_exit_status = ft_atoi(args[1]);
+		if (g_exit_status < 0 || g_exit_status > 255 || ft_strlen(args[1]) > 20)
+		{
+			ft_dprintf(STDERR, "exit\nexit: %s: numeric argument required\n", args[1]);
+			g_exit_status = 2;
+		}
+		else
+			ft_dprintf(STDOUT, "exit\n");
+	}
+	return (0);
+}
+
+/**
  * exit_builtin - Handles the exit command
  * @args: The arguments passed to the exit command
  * Return: Always returns 0, as this function does not return to the shell
@@ -26,19 +52,8 @@ int	exit_builtin(char **args)
 	}
 	else if (args[2] != NULL)
 		return (ft_dprintf(STDERR, "exit: too many arguments\n"), 1);
-	else if (ft_isnumber(args[1]) == 0)
-	{
-		ft_dprintf(STDERR, "exit\nexit: %s: numeric argument required\n",
-			args[1]);
-		g_exit_status = 2;
-	}
 	else
-	{
-		g_exit_status = ft_atoi(args[1]);
-		if (g_exit_status < 0 || g_exit_status > 255)
-			g_exit_status = 255;
-		ft_dprintf(STDOUT, "exit\n");
-	}
+		handle_exit_arg(args);
 	cleanup_and_exit(g_exit_status);
 	return (0);
 }
